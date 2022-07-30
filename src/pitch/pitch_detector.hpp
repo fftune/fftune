6e91dec5783505b77ed86c510244f3eef6eba083
Magnus Groß <magnus.gross@rwth-aachen.de>
2022-07-30 19:50:21 +0200

    Fix wrong binding
    We accidentally never ended up calling fast_comb

--- src/pitch/pitch_detector.hpp
+++ src/pitch/pitch_detector.hpp
@@ -38,7 +38,7 @@ public:
 	 *
 	 * This member holds the correct pitch detection backend, and must be chosen at compile time.
 	 */
-	typename std::conditional<T.algorithm == pitch_detection_method::Yin, yin, typename std::conditional<T.algorithm == pitch_detection_method::Fftune_Sfizz, fftune_sfizz, typename std::conditional<T.algorithm == pitch_detection_method::Fftune_Spectral, fftune_spectral, typename std::conditional<T.algorithm == pitch_detection_method::Fftune_Spectral, fast_comb, typename std::conditional<T.algorithm == pitch_detection_method::Double_Fft, double_fft, typename std::conditional<T.algorithm == pitch_detection_method::Yin_Patient, yin_patient, schmitt_trigger>::type>::type>::type>::type>::type>::type method {T};
+	typename std::conditional<T.algorithm == pitch_detection_method::Yin, yin, typename std::conditional<T.algorithm == pitch_detection_method::Fftune_Sfizz, fftune_sfizz, typename std::conditional<T.algorithm == pitch_detection_method::Fftune_Spectral, fftune_spectral, typename std::conditional<T.algorithm == pitch_detection_method::Fast_Comb, fast_comb, typename std::conditional<T.algorithm == pitch_detection_method::Double_Fft, double_fft, typename std::conditional<T.algorithm == pitch_detection_method::Yin_Patient, yin_patient, schmitt_trigger>::type>::type>::type>::type>::type>::type method {T};
 	/**
 	 * @brief Performs pitch detection
 	 *
