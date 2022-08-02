e1acde4d2cf1ca4ed07b5ac6ce2014aececf5ab8
Magnus Groß <magnus.gross@rwth-aachen.de>
2022-08-02 16:19:53 +0200

    Fix out of bounds read

--- src/fft/bin.cpp
+++ src/fft/bin.cpp
@@ -72,12 +72,12 @@ bins discrete_derivative(const bins &b) {
 float bins_distance(const bins &a, const bins &b) {
 	float result = 0.f;
 	// lower value means more similar
-	constexpr int neighbors = 10;
+	constexpr int neighbors = 40;
 	for (int i = 0; i < a.size(); ++i) {
 		float mean_a = 0.f;
 		float mean_b = 0.f;
 		size_t count = 0;
-		for (int j = std::min(0, i - neighbors); j < std::max(i + 10, static_cast<int>(a.size())); ++j) {
+		for (int j = std::max(0, i - neighbors); j < std::min(i + 10, static_cast<int>(a.size())); ++j) {
 			mean_a += a[j].magnitude;
 			mean_b += b[j].magnitude;
 			++count;
@@ -87,7 +87,7 @@ float bins_distance(const bins &a, const bins &b) {
 
 		float standard_deviation_a = 0.f;
 		float standard_deviation_b = 0.f;
-		for (int j = std::min(0, i - neighbors); j < std::max(i + 10, static_cast<int>(a.size())); ++j) {
+		for (int j = std::max(0, i - neighbors); j < std::min(i + 10, static_cast<int>(a.size())); ++j) {
 			float diff = a[j].magnitude - mean_a;
 			standard_deviation_a += diff * diff;
 
